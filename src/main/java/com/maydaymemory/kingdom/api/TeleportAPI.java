13601b8ec78245e6ed3a74db5a388a65ae52a892
286799714 <41233300+286799714@users.noreply.github.com>
2023-06-21 02:23:35 +0800

    Setup CircleCI

--- src/main/java/com/maydaymemory/kingdom/api/TeleportAPI.java
+++ src/main/java/com/maydaymemory/kingdom/api/TeleportAPI.java
@@ -1,6 +1,7 @@
 package com.maydaymemory.kingdom.api;
 
 import com.maydaymemory.kingdom.Reference;
+import com.maydaymemory.kingdom.core.config.ConfigInject;
 import com.maydaymemory.kingdom.model.region.PrivateRegion;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
@@ -10,48 +11,61 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 public class TeleportAPI {
-    private static final class InstanceHolder{
+    private static final class InstanceHolder {
         static final TeleportAPI instance = new TeleportAPI();
     }
 
-    public static TeleportAPI getInstance(){
+    public static TeleportAPI getInstance() {
         return TeleportAPI.InstanceHolder.instance;
     }
 
-    private ItemStack item;
+    @ConfigInject
+    private static Configuration config;
+
+    private ItemStack teleportItem;
     private int per;
     private int cost;
     private int world;
-    private TeleportAPI(){
-        Configuration config= Bukkit.getPluginManager().getPlugin(Reference.PLUGIN_NAME).getConfig();
-        per=config.getInt("teleport.per");
-        cost=config.getInt("teleport.cost");
-        world=config.getInt("teleport.world");
-        item=new ItemStack(Material.matchMaterial(config.getString("teleport.item.material")));
-        ItemMeta meta=item.getItemMeta();
+
+    private TeleportAPI() {
+        per = config.getInt("teleport.per");
+        cost = config.getInt("teleport.cost");
+        world = config.getInt("teleport.world");
+        teleportItem = new ItemStack(Material.matchMaterial(config.getString("teleport.item.material")));
+        ItemMeta meta = teleportItem.getItemMeta();
         meta.setDisplayName(config.getString("teleport.item.name"));
         meta.setLore(config.getStringList("teleport.item.lore"));
-        item.setItemMeta(meta);
+        teleportItem.setItemMeta(meta);
     }
 
-    public int calculateDistance(PrivateRegion region1,PrivateRegion region2){
-        return (int)Math.pow(Math.pow(region1.getMainChunk().getX()-region2.getMainChunk().getX(),2)
-                        +Math.pow(region1.getMainChunk().getZ()-region2.getMainChunk().getZ(),2)
-                ,0.5);
+    public int calculateDistance(PrivateRegion region1, PrivateRegion region2) {
+        return (int) Math.pow(Math.pow(region1.getMainChunk().getX() - region2.getMainChunk().getX(), 2)
+                        + Math.pow(region1.getMainChunk().getZ() - region2.getMainChunk().getZ(), 2)
+                , 0.5);
     }
 
-    public int calculateCost(PrivateRegion start, PrivateRegion to){
-        int result=calculateDistance(start,to);
-        result=(result-(result%per))/per*cost;
-        if(start.getMainChunk().getWorld()!=to.getMainChunk().getWorld()){
-            result+=world;
+    public int calculateCost(PrivateRegion start, PrivateRegion to) {
+        int result = calculateDistance(start, to);
+        result = (result - (result % per)) / per * cost;
+        if (start.getMainChunk().getWorld() != to.getMainChunk().getWorld()) {
+            result += world;
         }
         return result;
     }
 
     public ItemStack getTeleportItem() {
-        return item;
+        return teleportItem;
     }
 
+    public int getPer() {
+        return per;
+    }
 
+    public int getCost() {
+        return cost;
+    }
+
+    public int getWorld() {
+        return world;
+    }
 }
---
+++ src/main/java/com/maydaymemory/kingdom/dao/bean/PrivateRegionBean.java
@@ -0,0 +1,56 @@
+package com.maydaymemory.kingdom.dao.bean;
+
+import com.maydaymemory.kingdom.core.util.Pair;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.UUID;
+
+public class PrivateRegionBean extends AdministrativeRegionBean {
+    protected UUID owner;
+    protected Set<UUID> resident = new HashSet<>();
+    protected Set<Pair<String, Pair<Integer, Integer>>> residentialChunks = new TreeSet<>();
+    protected Pair<String, Pair<Integer, Integer>> mainChunk = null;
+    private String name;
+
+    public UUID getOwner() {
+        return owner;
+    }
+
+    public void setOwner(UUID owner) {
+        this.owner = owner;
+    }
+
+    public Set<UUID> getResident() {
+        return resident;
+    }
+
+    public void setResident(Set<UUID> resident) {
+        this.resident = resident;
+    }
+
+    public Set<Pair<String, Pair<Integer, Integer>>> getResidentialChunks() {
+        return residentialChunks;
+    }
+
+    public void setResidentialChunks(Set<Pair<String, Pair<Integer, Integer>>> residentialChunks) {
+        this.residentialChunks = residentialChunks;
+    }
+
+    public Pair<String, Pair<Integer, Integer>> getMainChunk() {
+        return mainChunk;
+    }
+
+    public void setMainChunk(Pair<String, Pair<Integer, Integer>> mainChunk) {
+        this.mainChunk = mainChunk;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
