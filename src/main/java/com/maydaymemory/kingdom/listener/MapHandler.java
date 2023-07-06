48c8d5f836c112bcaa5945ed7243cd35512916ba
286799714 <41233300+286799714@users.noreply.github.com>
2023-07-06 17:37:50 +0800

    Update MapHandler.java

--- src/main/java/com/maydaymemory/kingdom/listener/MapHandler.java
+++ src/main/java/com/maydaymemory/kingdom/listener/MapHandler.java
@@ -1,8 +1,9 @@
 package com.maydaymemory.kingdom.listener;
 
+import com.maydaymemory.kingdom.api.PrivateRegionAPI;
 import com.maydaymemory.kingdom.core.config.ConfigInject;
-import com.maydaymemory.kingdom.model.player.PlayerInfo;
-import com.maydaymemory.kingdom.model.player.PlayerInfoManager;
+import com.maydaymemory.kingdom.model.chunk.ChunkInfo;
+import com.maydaymemory.kingdom.model.chunk.ChunkInfoManager;
 import com.maydaymemory.kingdom.model.region.PrivateRegion;
 import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
@@ -18,6 +19,9 @@ import org.bukkit.map.MapView;
 
 import javax.annotation.Nonnull;
 import java.awt.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class MapHandler implements Listener {
     @ConfigInject
@@ -25,27 +29,47 @@ public class MapHandler implements Listener {
 
     @EventHandler
     public void onMapInitialize(MapInitializeEvent event){
-        event.getMap().addRenderer(new MyMapRenderer());
-        Bukkit.getLogger().info("test");
+        if(config.getBoolean("private-region.render-map", true)) event.getMap().addRenderer(new MyMapRenderer());
     }
 
     private static class MyMapRenderer extends MapRenderer {
 
+        private final Map<String, Integer> colorMap = new HashMap<>();
+
+        private final Color[] plate = {Color.RED, Color.BLUE, Color.GREEN, Color.YELLOW};
+
         @Override
         public void render(@Nonnull MapView mapView, @Nonnull MapCanvas mapCanvas, @Nonnull Player player) {
-            PlayerInfo info = PlayerInfoManager.getInstance().getOrCreate(player.getUniqueId());
-            if(!info.getPrivateRegions().isEmpty()){
-                PrivateRegion region = info.getPrivateRegions().stream().findFirst().orElseThrow(()->new NullPointerException(""));
+            Set<PrivateRegion> regionSet = PrivateRegionAPI.getInstance().getAllPrivateRegion();
+            for(PrivateRegion region : regionSet) {
                 region.getResidentialChunks().stream()
                         .map(pair -> {
                             World world = Bukkit.getWorld(pair.getFormer());
-                            if(world == null) return null;
+                            if (world == null) return null;
                             return world.getChunkAt(pair.getLatter().getFormer(), pair.getLatter().getLatter());
                         })
-                        .forEach(chunk->{
-                            if(chunk !=null) renderChunk(mapView, mapCanvas, player, chunk, Color.red);
+                        .forEach(chunk -> {
+                            if (chunk != null) renderChunk(mapView, mapCanvas, player, chunk, Color.red);
                         });
             }
+        }
+
+        private Integer getColor(Chunk chunk){
+            /*
+            ChunkInfo info = ChunkInfoManager.getInstance().getOrCreate(chunk);
+            if(!info.isClaimed()) return null;
+            World world = Bukkit.getWorld(info.getWorld());
+            if(world == null) return null;
+            Chunk[] nearbyChunks = {world.getChunkAt(chunk.getX(), chunk.getZ() + 1),
+                                    world.getChunkAt(chunk.getX(), chunk.getZ() - 1),
+                                    world.getChunkAt(chunk.getX() + 1, chunk.getZ()),
+                                    world.getChunkAt(chunk.getX() - 1, chunk.getZ())};
+            for(Chunk nearbyChunk : nearbyChunks){
+
+            }
+
+             */
+            return 0;
         }
 
         private void renderChunk(@Nonnull MapView mapView, @Nonnull MapCanvas mapCanvas, @Nonnull Player player, Chunk chunk, Color color){
